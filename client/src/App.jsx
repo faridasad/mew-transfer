705fb4274353ae0b2c45f13c8b9781f0b37def3e
faridasad <asadlifaridd@gmail.com>
2022-09-06 19:45:08 +0400

    Change the way sending data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,20 +6,28 @@ function App() {
   const [filename, setFilename] = useState("");
   const [itemlink, setItemlink] = useState("");
   const [nextStep, setNextStep] = useState(false);
+  const data = new FormData();
 
-  const postData = (e) => {
-    const data = new FormData();
-    data.append("file", e.target.files[0]);
-    setFilename(e.target.files[0].name);
+  const uploadFile = (files) => {
+    Array.from(files).forEach((file) => {
+      data.append("file", file);
+    });
 
-    axios
-      .post("http://localhost:3000/upload", data)
-      .then((res) => setItemlink(res.data.fileLink));
+    for (const value of data.values()) {
+      console.log(value);
+    }
   };
 
-  const getLink = (e) => {
-    e.preventDefault();
-    setNextStep(true);
+  const addFile = (e) => {
+    const files = e.target.files;
+    uploadFile(files);
+  };
+
+  const getLink = () => {
+    axios
+      .post("http://localhost:3000/upload", data)
+      .then(res => console.log(res.statusText))
+      /* .then((res) => setItemlink(res.data.fileLink)); */
   };
 
   return (
@@ -38,7 +46,7 @@ function App() {
         <form className="upload-container" encType="multipart/form-data">
           <div className="add-files">
             <input
-              onChange={postData}
+              onChange={addFile}
               type="file"
               id="addFile"
               multiple
@@ -79,7 +87,7 @@ function App() {
             </div>
           )}
           {!nextStep && (
-            <button type="submit" onClick={getLink}>
+            <button type="submit" onClick={(e) => {e.preventDefault(); getLink()} }>
               Get a link
             </button>
           )}
