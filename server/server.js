75eaa224e2e8475f57538a5d4c0f346615aabb00
faridasad <asadlifaridd@gmail.com>
2022-09-06 19:45:23 +0400

    Handle multiple files

--- server/server.js
+++ server/server.js
@@ -2,18 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
 const mongoose = require("mongoose");
-const File = require("./models/File")
+const File = require("./models/File");
 
-/* const storage = multer.diskStorage({
-    destination: (req, file, callBack) => {
-        callBack(null, 'uploads')
-    },
-    filename: (req, file, callBack) => {
-        callBack(null, `${file.originalname}`)
-    }
-  }) */
-
-const upload = multer({ dest: "uploads" })
+const upload = multer({ dest: "./uploads" });
 
 const app = express();
 
@@ -23,32 +14,34 @@ app.use(
     extended: true,
   })
 );
-app.use(express.json())
+app.use(express.json());
 
-
-mongoose.connect("mongodb+srv://nonwhm:bruh123@filesharing.vh4r0an.mongodb.net/?retryWrites=true&w=majority")
-
-
+mongoose.connect(
+  "mongodb+srv://nonwhm:bruh123@filesharing.vh4r0an.mongodb.net/?retryWrites=true&w=majority"
+);
 
 app.get("/upload", (req, res) => {
   res.send("hi");
 });
 
-app.post("/upload", upload.single('file'), async (req, res) => {
-  const fileData = {
-    path: req.file.path,
-    originalName: req.file.originalname
-  }
+app.post("/upload", upload.array("file"), async (req, res) => {
+  if (req.files.length === 1) {
+    const fileData = {
+      path: req.files[0].path,
+      originalName: req.files[0].originalname,
+    };
 
-  const file = await File.create(fileData)
-  res.send({fileLink: `file/${file.id}`})
+    const file = await File.create(fileData);
+    res.send({ fileLink: `file/${file.id}` });
+  }else if(req.files.length > 1){
+    // add the files to folder and download // todo
+  }
 });
 
 app.get("/file/:id", async (req, res) => {
-  const file = await File.findById(req.params.id)
-  await file.save()
-  res.download(file.path, file.originalName)
-})
-
+  const file = await File.findById(req.params.id);
+  await file.save();
+  res.download(file.path, file.originalName);
+});
 
 app.listen(3000);
